dfeef95ce0939d0f49aa38c888012a919caca23f
Peter Love <p.love@lancaster.ac.uk>
2020-02-09 22:20:25 +0100

    version bump

--- runpilot2-wrapper.sh
+++ runpilot2-wrapper.sh
@@ -4,7 +4,7 @@
 #
 # https://google.github.io/styleguide/shell.xml
 
-VERSION=20200130a-pilot2next
+VERSION=20200209a-pilot2
 
 function err() {
   dt=$(date --utc +"%Y-%m-%d %H:%M:%S,%3N [wrapper]")
