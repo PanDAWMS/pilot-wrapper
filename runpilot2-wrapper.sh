076c305858cf67c89ff0b916b821ccf51138bdef
Peter Love <p.love@lancaster.ac.uk>
2020-09-29 14:37:34 +0200

    use python2 explicitly for compatible with CentOS8

--- runpilot2-wrapper.sh
+++ runpilot2-wrapper.sh
@@ -4,7 +4,7 @@
 #
 # https://google.github.io/styleguide/shell.xml
 
-VERSION=20200907a-next
+VERSION=20200929a-next
 
 function err() {
   dt=$(date --utc +"%Y-%m-%d %H:%M:%S,%3N [wrapper]")
@@ -49,10 +49,10 @@ function get_workdir {
 }
 
 function check_python2() {
-  pybin=$(which python)
+  pybin=$(which python2)
   if [[ $? -ne 0 ]]; then
-    log "FATAL: python not found in PATH"
-    err "FATAL: python not found in PATH"
+    log "FATAL: python2 not found in PATH"
+    err "FATAL: python2 not found in PATH"
     if [[ -z "${PATH}" ]]; then
       log "In fact, PATH env var is unset mon amie"
       err "In fact, PATH env var is unset mon amie"
