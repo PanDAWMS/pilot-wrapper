03188f5c3793e74e59de55b84bc98a624f70add0
Peter Love <p.love@lancaster.ac.uk>
2022-08-30 12:07:50 +0200

    setup lsst

--- rubin-wrapper.sh
+++ rubin-wrapper.sh
@@ -4,7 +4,7 @@
 #
 # https://google.github.io/styleguide/shell.xml
 
-VERSION=20220715a-rubin
+VERSION=20220829a-rubin
 
 function err() {
   dt=$(date --utc +"%Y-%m-%d %H:%M:%S,%3N [wrapper]")
@@ -106,12 +106,9 @@ function check_cvmfs() {
 function setup_lsst() {
   log "Sourcing: /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v0.0.2-dev/setup_panda.sh"
   source /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v0.0.2-dev/setup_panda.sh
-  log "INFO: temp using ALRB to setup rucio"
-  [ -z "$ALRB_noGridMW" ] && export ALRB_noGridMW=YES
-  ALRB_PYTHON_OPT="-3"
-  source $ATLAS_LOCAL_ROOT_BASE/user/atlasLocalSetup.sh --quiet $ALRB_PYTHON_OPT
-  export RUCIO_ACCOUNT=rubin
-  lsetup rucio 
+  log "Sourcing: /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v0.0.2-dev/conda/install/bin/activate"
+  source /cvmfs/sw.lsst.eu/linux-x86_64/panda_env/v0.0.2-dev/conda/install/bin/activate
+  log "TODO: rucio setup"
   log "rucio whoami: $(rucio whoami)"
 }
 
