e2e8ee2255a85a5ca80d5f555748c972000b72ef
Peter Love <p.love@lancaster.ac.uk>
2023-01-13 14:30:08 +0100

    chk for mq

--- runpilot2-wrapper.sh
+++ runpilot2-wrapper.sh
@@ -4,7 +4,7 @@
 #
 # https://google.github.io/styleguide/shell.xml
 
-VERSION=20220927b-next
+VERSION=20230113a-next
 
 function err() {
   dt=$(date --utc +"%Y-%m-%d %H:%M:%S,%3N [wrapper]")
@@ -635,6 +635,8 @@ function main() {
   echo "id:" $(id)
   echo "getopt:" $(getopt -V 2>/dev/null)
   echo "jq:" $(jq --version 2>/dev/null)
+  echo "mq:" $(which mosquitto_pub 2>/dev/null)
+  echo "mq:" $(mosquitto_pub --help | grep libmosquitto 2>/dev/null)
   if [[ -r /proc/version ]]; then
     echo "/proc/version:" $(cat /proc/version)
   fi
