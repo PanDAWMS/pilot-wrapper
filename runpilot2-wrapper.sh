fd3a8674f93ee9fe86e1dd7229c3e7add5a5ca9f
Peter Love <p.love@lancaster.ac.uk>
2024-10-31 11:17:59 +0100

    note exit code interpretation

--- runpilot2-wrapper.sh
+++ runpilot2-wrapper.sh
@@ -4,7 +4,7 @@
 #
 # https://google.github.io/styleguide/shell.xml
 
-VERSION=20241023a-next
+VERSION=20241031z-next
 
 function err() {
   dt=$(date --utc +"%Y-%m-%d %H:%M:%S,%3N [wrapper]")
@@ -499,6 +499,11 @@ function trap_handler() {
 }
 
 function sortie() {
+  # Currently Harvester interprets exit-codes as following:
+  #
+  #         1: "wrapper fault",
+  #         2: "wrapper killed stuck pilot",
+  #        64: "wrapper got cvmfs repos issue",
   ec=$1
   if [[ $ec -eq 0 ]]; then
     state=wrapperexiting
