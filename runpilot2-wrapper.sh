1e366f76ba2866c6ab44a59d7f07b98f4efc86c2
Hrushikesh Kothem <hrushikesh.kothem@thoughtworks.com>
2026-06-17 11:40:08 +0530

    Implement queuedata URL option in script
    Add support for queuedata URL in runpilot2-wrapper.sh

--- runpilot2-wrapper.sh
+++ runpilot2-wrapper.sh
@@ -1184,6 +1184,11 @@ case $key in
     shift
     shift
     ;;
+    --queuedata-url)
+    queuedata_url="$2"
+    shift
+    shift
+    ;;
     -i)
     iarg="$2"
     shift
@@ -1236,6 +1241,8 @@ pilotargs="$@"
 
 if [[ -f queuedata.json ]]; then
   cricurl="file://${PWD}/queuedata.json"
+elif [[ -n "${queuedata_url}" ]]; then
+  cricurl="${queuedata_url}"
 else
   cricurl="http://pandaserver.cern.ch:25085/cache/schedconfig/${qarg}.all.json"
 fi
